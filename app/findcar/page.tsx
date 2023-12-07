fa3a138c3baff4beae5ff07b1a952327376ea746
jeric realubit <jericrealubit@gmail.com>
2023-12-07 21:46:29 +1300

    all good

--- app/findcar/page.tsx
+++ app/findcar/page.tsx
@@ -4,13 +4,26 @@ import { Envs } from "@/utils/config";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
+const isValidHttpUrl = (urlInput: string) => {
+  let url;
+
+  try {
+    url = new URL(urlInput);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+};
+
 const Findcar = () => {
   const [carUrl, setCarUrl] = useState("/images/noimage.jpeg");
   const [carTypes, setCarTypes] = useState([]);
   const [isLoading, setLoading] = useState(false);
-  const [isThingking, setThingking] = useState(false);
   const [carInput, setCarInput] = useState("");
   const [carOutput, setCarOutput] = useState<string[]>([]);
+  const [visionError, setVisionError] = useState(false);
+  const [isValidUrl, setIsValidUrl] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:4000/data")
@@ -27,25 +40,22 @@ const Findcar = () => {
         result.push(haystack[i]);
       }
     }
-    console.log({ haystack });
-    console.log({ niddle });
-    console.log({ result });
     setCarOutput(result);
-    console.log({ carOutput });
     return result;
   };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    setLoading(true);
-    setThingking(true);
+    setCarOutput([]);
     setCarInput("");
-    const pkey = Envs.PREDICTION_KEY;
-    const purl = Envs.PREDICTION_URL;
-    const submitData = { url: carUrl };
 
-    try {
+    if (isValidUrl) {
+      setLoading(true);
+      const pkey = Envs.PREDICTION_KEY;
+      const purl = Envs.PREDICTION_URL;
+      const submitData = { url: carUrl };
+
       const res = await fetch(purl || "", {
         method: "POST",
         body: JSON.stringify(submitData),
@@ -53,31 +63,33 @@ const Findcar = () => {
           "content-type": "application/json",
           "Prediction-Key": pkey || "",
         },
-      });
-      const data = await res.json();
-      const tag = data.predictions[0].tagName;
+      })
+        .then((response) => {
+          if (response.status >= 400 && response.status < 600) {
+            console.log(response);
+            throw new Error(`Request failed with status ${response.status}`);
+          } else {
+            return response;
+          }
+        })
+        .catch((error) => {
+          // Your error is here!
+          console.log(error);
+        });
 
-      setCarInput(tag);
-      setThingking(false);
-
-      const searchResult = getMatchType(carTypes, tag);
-      console.log({ searchResult });
-      setCarOutput(searchResult);
-      //setCarOutput(result);
-      console.log({ carOutput });
-      console.log(typeof carOutput);
-      const carOutputR = Object.entries(carOutput);
-      console.log({ carOutputR });
-      console.log(typeof carOutputR);
-      setLoading(false);
-
-      if (res.ok) {
-        console.log("Yeai!");
+      if (!res?.ok) {
+        throw new Error(`Request failed with status ${res?.status}`);
       } else {
-        console.log("Oops! Something is wrong.");
+        const data = await res.json();
+        console.log({ data });
+        const tag = data.predictions[0].tagName;
+        setCarInput(tag);
+        const searchResult = getMatchType(carTypes, tag);
+        console.log({ searchResult });
+        setLoading(false);
       }
-    } catch (error) {
-      console.log(error);
+    } else {
+      setIsValidUrl(false);
     }
   };
 
@@ -89,25 +101,12 @@ const Findcar = () => {
 
   return (
     <main>
-      <div className=" flex flex-col justify-center items-center w-full p-8 ">
-        <Image
-          loader={(l: Loader): string =>
-            `${l.src}?w=${l.width}&q=${l.quality || 75}`
-          }
-          src={carUrl}
-          alt="car image"
-          width={200}
-          height={200}
-          priority={false}
-        />
-        <h1 className=" w-full text-center m-4 font-semibold text-lg ">
-          Find similar car type
-        </h1>
+      <div className="max-w-5xl flex flex-wrap justify-center items-center w-full ">
         <form
-          className=" flex w-full flex-col justify-center items-center "
+          className=" flex justify-center items-center "
           onSubmit={handleSubmit}
         >
-          <div className=" flex w-full justify-center items-center gap-4 ">
+          <div className=" flex justify-center items-center gap-4 ">
             <input
               type="text"
               name="url"
@@ -118,6 +117,11 @@ const Findcar = () => {
                   : undefined
               }
               className=" border p-2 px-4 rounded outline-none "
+              onKeyUp={(e) => {
+                isValidHttpUrl(e.target?.value)
+                  ? setIsValidUrl(true)
+                  : setIsValidUrl(false);
+              }}
             />
             <button
               type="submit"
@@ -126,15 +130,65 @@ const Findcar = () => {
               Submit
             </button>
             <div className="w-24">
-              {isThingking && <div>...</div>}
+              {isLoading && <div>...</div>}
               {carInput}
             </div>
           </div>
         </form>
+        <div className="flex flex-col">
+          <Image
+            loader={(l: Loader): string =>
+              `${l.src}?w=${l.width}&q=${l.quality || 75}`
+            }
+            src={carUrl}
+            alt="car image"
+            width={200}
+            height={200}
+            priority={false}
+            className="max-h-40"
+          />
+        </div>
       </div>
       <div>
-        {isLoading && <p>Loading...</p>}
-        <div className="flex flex-col">
+        {visionError && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong className="font-bold">Error!</strong>
+            <span className="block sm:inline">{visionError}</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg
+                className="fill-current h-6 w-6 text-red-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
+        )}
+        {isLoading && (
+          <Image
+            className="mx-auto"
+            src="/images/hero-moving-car.gif"
+            width={200}
+            height={200}
+          />
+        )}
+
+        <div>
+          {!isValidUrl && (
+            <div className="error">Please enter a valid url.</div>
+          )}
+          {carInput === "Negative" && (
+            <div className="error">This is not a car image</div>
+          )}
+        </div>
+
+        <div className="flex flex-wrap">
           {carOutput.map((car) => (
             <Image
               key={car.id}
